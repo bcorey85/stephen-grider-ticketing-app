b18cb785ed2fadfc7e30387b79e0966e2f897405
Brandon Corey <bcorey85@gmail.com>
2020-07-24 13:10:02 -0500

    add expiration service

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,74 @@
+import { Message } from 'node-nats-streaming';
+import { ExpirationCompleteListener } from '../expiration-complete-listener';
+import { ExpirationCompleteEvent } from '@bctickets/common';
+import { natsWrapper } from '../../../nats-wrapper';
+import mongoose from 'mongoose';
+import { Ticket } from '../../../models/ticket';
+import { Order, OrderStatus } from '../../../models/order';
+
+const setup = async () => {
+	const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+	const ticket = Ticket.build({
+		id: mongoose.Types.ObjectId().toHexString(),
+		title: 'concert',
+		price: 20
+	});
+	await ticket.save();
+
+	const order = Order.build({
+		status: OrderStatus.Created,
+		userId: 'al;sdfkj',
+		expiresAt: new Date(),
+		ticket
+	});
+	await order.save();
+
+	const data: ExpirationCompleteEvent['data'] = {
+		orderId: order.id
+	};
+
+	// @ts-ignore
+	const msg: Message = {
+		ack: jest.fn()
+	};
+
+	return {
+		listener,
+		data,
+		msg,
+		order,
+		ticket
+	};
+};
+
+it('updates the order status to cancelled', async () => {
+	const { listener, data, msg, order, ticket } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	const updatedOrder = await Order.findById(order.id);
+	expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('emit an OrderCancelled event', async () => {
+	const { listener, data, msg, order, ticket } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+	const eventData = JSON.parse(
+		(natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+	);
+
+	expect(eventData.id).toEqual(order.id);
+});
+
+it('ack the message', async () => {
+	const { listener, data, msg, order, ticket } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(msg.ack).toHaveBeenCalled();
+});
--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -13,7 +13,6 @@ class NatsWrapper {
 
 	connect(clusterId: string, clientId: string, url: string) {
 		this._client = nats.connect(clusterId, clientId, { url });
-		console.log(url);
 
 		return new Promise((resolve, reject) => {
 			this.client.on('connect', () => {
